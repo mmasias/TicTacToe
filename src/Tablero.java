eb7f3555b712cd6100f2326668cfe6f85717541f
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-03-19 21:14:30 +0100

    Terminado... POR FIN

---
+++ src/Tablero.java
@@ -0,0 +1,83 @@
+
+public class Tablero {
+
+        private int[][] tablero;
+        private char[] formas;
+        private int filas;
+        private int columnas;
+
+        public Tablero(int[] dimensiones, char[] formas) {
+                this.formas = formas;
+                this.filas = dimensiones[0];
+                this.columnas = dimensiones[1];
+                establecerDimensiones();
+                poblarTablero();
+        }
+
+        private void poblarTablero() {
+                for (int i = 0; i < tablero.length; i++) {
+                        for (int j = 0; j < tablero[i].length; j++) {
+                                tablero[i][j] = getFormaVacia();
+                        }
+                }
+        }
+
+        private void establecerDimensiones() {
+                tablero = new int[getColumnas()][getFilas()];
+        }
+
+        public int getFilas() {
+                return filas;
+        }
+
+        public int getColumnas() {
+                return columnas;
+        }
+
+        public void mostrar() {
+                for (int i = 0; i < tablero.length; i++) {
+                        for (int j = 0; j < tablero[i].length; j++) {
+                                System.out.print(parseFichas(tablero[i][j]) + " ");
+                        }
+                        System.out.println();
+                }
+        }
+
+        private char parseFichas(int ficha) {
+                if (ficha == getFormaVacia()) return '_';
+                return formas[ficha];
+        }
+
+        public void colocarFicha(Jugador jugador) {
+                Coordenada jugada = jugador.getJugada(this);
+                while (tablero[jugada.getFila()][jugada.getColumna()] != getFormaVacia()) {
+                        System.out.println("Casilla ocupada");
+                        jugada = jugador.getJugada(this);
+                }
+                tablero[jugada.getFila()][jugada.getColumna()] = jugador.getSimbolo();
+                jugador.setFichasEnMano(jugador.getFichasEnMano() - 1);
+        }
+
+        public void cogerFicha(Jugador jugador) {
+                Coordenada jugada = jugador.getJugada(this);
+                while (tablero[jugada.getFila()][jugada.getColumna()] == getFormaVacia()) {
+                        System.out.println("Casilla vacía");
+                        jugada = jugador.getJugada(this);
+                }
+                while (tablero[jugada.getFila()][jugada.getColumna()] != jugador.getSimbolo()) {
+                        System.out.println("No puedes coger una ficha que no es tuya");
+                        jugada = jugador.getJugada(this);
+                }
+                tablero[jugada.getFila()][jugada.getColumna()] = getFormaVacia();
+                jugador.setFichasEnMano(jugador.getFichasEnMano() + 1);
+        }
+
+        public int getFicha(int i, int j) {
+                return tablero[i][j];
+        }
+
+        public int getFormaVacia() {
+                return -1;
+        }
+
+}
--- src/TresEnRaya.java
+++ src/TresEnRaya.java
@@ -2,18 +2,22 @@
 public class TresEnRaya {
 
         private Tablero tablero;
-        private Jugador[] jugadores;
-        private Turno turno;
-        private Ficha[] fichas;
+        private static final int[] DIMENSIONES = { 3, 3 };
 
-        private final int NUM_JUGADORES = 2;
-        private final char[] SIMBOLO_FICHAS = {'X', 'O'};
+        private Jugador[] jugadores;
+        private final int NUMERO_JUGADORES = 2;
+        private final char[] SIMBOLO_FICHAS = { 'X', 'O' };
+        private final int FICHAS_POR_JUGADOR = 3;
+
+        private Turno turno;
 
         public TresEnRaya() {
-                tablero = new Tablero();
-                jugadores = new Jugador[2];
+                tablero = new Tablero(DIMENSIONES, SIMBOLO_FICHAS);
 
-                fichas = new Ficha[2];
+                jugadores = new Jugador[NUMERO_JUGADORES];
+                for (int i = 0; i < jugadores.length; i++) {
+                        jugadores[i] = new Jugador("Jugador " + (i + 1), i);
+                }
 
                 turno = new Turno(jugadores);
         }
@@ -22,10 +26,24 @@ public class TresEnRaya {
                 repartirFichas();
                 do {
                         tablero.mostrar();
-                        jugadores[turno.getJugador()].jugar(tablero);
+
+                        if (jugadores[turno.getJugador()].noTieneFichas()) {
+                                System.out.println(jugadores[turno.getJugador()].getNombre() + " no tiene más fichas. Mueva una del tablero.");
+                                tablero.cogerFicha(jugadores[turno.getJugador()]);
+                                tablero.mostrar();
+                        }
+                        tablero.colocarFicha(jugadores[turno.getJugador()]);
+
+                        turno.siguienteJugador();
                 } while (!hayTresEnRaya(tablero));
                 tablero.mostrar();
                 System.out.println("¡¡¡" + jugadores[turno.getJugador()].getNombre() + " ha ganado!!!");
+        }
+
+        private void repartirFichas() {
+                for (int i = 0; i < jugadores.length; i++) {
+                        jugadores[i].setFichasEnMano(FICHAS_POR_JUGADOR);
+                }
         }
 
         private boolean hayTresEnRaya(Tablero tablero) {
@@ -34,21 +52,20 @@ public class TresEnRaya {
 
         private boolean hayFila(Tablero tablero) {
                 for (int i = 0; i < tablero.getFilas(); i++) {
-                        boolean hayFila = tablero.getFicha(i, 0) != 0 
-                                && tablero.getFicha(i, 0).equals(tablero.getFicha(i, 1))
-                                && tablero.getFicha(i, 1).equals(tablero.getFicha(i, 2));
-                        if (hayFila) {
+                        boolean hayFila = tablero.getFicha(i, 0) != tablero.getFormaVacia()
+                                        && tablero.getFicha(i, 0) == tablero.getFicha(i, 1)
+                                        && tablero.getFicha(i, 1) == tablero.getFicha(i, 2);
+                        if (hayFila)
                                 return true;
-                        }
                 }
-                return true;
+                return false;
         }
 
         private boolean hayColumna(Tablero tablero) {
                 for (int i = 0; i < tablero.getColumnas(); i++) {
-                        boolean hayColumna = tablero.getFicha(0, i) != 0 
-                                && tablero.getFicha(0, i).equals(tablero.getFicha(1, i))
-                                && tablero.getFicha(1, i).equals(tablero.getFicha(2, i));
+                        boolean hayColumna = tablero.getFicha(0, i) != tablero.getFormaVacia()
+                                        && tablero.getFicha(0, i) == tablero.getFicha(1, i)
+                                        && tablero.getFicha(1, i) == tablero.getFicha(2, i);
                         if (hayColumna) {
                                 return true;
                         }
@@ -57,12 +74,13 @@ public class TresEnRaya {
         }
 
         private boolean hayDiagonal(Tablero tablero) {
-                return (tablero.getFicha(0, 0) != 0 
-                        && tablero.getFicha(0, 0).equals(tablero.getFicha(1, 1))
-                        && tablero.getFicha(1, 1).equals(tablero.getFicha(2, 2)))
-                        || (tablero.getFicha(0, 2) != 0 
-                        && tablero.getFicha(0, 2).equals(tablero.getFicha(1, 1))
-                        && tablero.getFicha(1, 1).equals(tablero.getFicha(2, 0)));
+                boolean primeraDiagonal = tablero.getFicha(0, 0) != tablero.getFormaVacia()
+                                && tablero.getFicha(0, 0) == tablero.getFicha(1, 1)
+                                && tablero.getFicha(1, 1) == tablero.getFicha(2, 2);
+                boolean segundaDiagonal = tablero.getFicha(0, 2) != tablero.getFormaVacia()
+                                && tablero.getFicha(0, 2) == tablero.getFicha(1, 1)
+                                && tablero.getFicha(1, 1) == tablero.getFicha(2, 0);
+                return primeraDiagonal || segundaDiagonal;
         }
 
 }
